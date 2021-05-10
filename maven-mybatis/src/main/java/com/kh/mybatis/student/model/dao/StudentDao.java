2e9c57bb798b184ff4d2e19b7d67672b64e04002
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-10 22:29:15 +0900

    maven

--- maven-mybatis/src/main/java/com/kh/mybatis/student/model/dao/StudentDao.java
+++ maven-mybatis/src/main/java/com/kh/mybatis/student/model/dao/StudentDao.java
@@ -1,5 +1,8 @@
 package com.kh.mybatis.student.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.kh.mybatis.student.model.vo.Student;
@@ -7,5 +10,21 @@ import com.kh.mybatis.student.model.vo.Student;
 public interface StudentDao {
 
 	int insertStudent(SqlSession session, Student student);
+
+	int insertStudentMap(SqlSession session, Map<String, Object> student);
+
+	int selectStudentCount(SqlSession session);
+
+	Student selectOneStudent(SqlSession session, int no);
+
+	Map<String, Object> selectOneStudentMap(SqlSession session, int no);
+
+	int updateStudent(SqlSession session, Student student);
+
+	int deleteStudent(SqlSession session, int no);
+
+	List<Student> selectStudentList(SqlSession session);
+
+	List<Map<String, Object>> selectStudentMapList(SqlSession session);
 
 }
--- maven-mybatis/src/main/java/com/kh/mybatis/student/model/service/StudentServiceImpl.java
+++ maven-mybatis/src/main/java/com/kh/mybatis/student/model/service/StudentServiceImpl.java
@@ -2,6 +2,9 @@ package com.kh.mybatis.student.model.service;
 
 import static com.kh.mybatis.common.MybatisUtils.getSqlSession;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.kh.mybatis.student.model.dao.StudentDao;
@@ -31,4 +34,98 @@ public class StudentServiceImpl implements StudentService {
 		}
 		return result;
 	}
+
+	@Override
+	public int insertStudentMap(Map<String, Object> student) {
+		int result = 0;
+		SqlSession session = getSqlSession();
+		try {
+			result = studentDao.insertStudentMap(session, student);
+			session.commit();
+		} catch(Exception e) {
+			session.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	@Override
+	public int selectStudentCount() {
+		SqlSession session = getSqlSession();
+		int total = studentDao.selectStudentCount(session);
+		session.close();
+		return total;
+	}
+
+	@Override
+	public Student selectOneStudent(int no) {
+		SqlSession session = getSqlSession();
+		Student student = studentDao.selectOneStudent(session, no);
+		session.close();
+		return student;
+	}
+
+	@Override
+	public Map<String, Object> selectOneStudentMap(int no) {
+		SqlSession session = getSqlSession();
+		Map<String, Object> student = studentDao.selectOneStudentMap(session, no);
+		session.close();
+		return student;
+	}
+
+	@Override
+	public int updateStudent(Student student) {
+		int result = 0;
+		SqlSession session = getSqlSession();
+		try {
+			result = studentDao.updateStudent(session, student);
+			session.commit();
+		} catch(Exception e) {
+			session.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	@Override
+	public int deleteStudent(int no) {
+		int result = 0;
+		SqlSession session = getSqlSession();
+		try {
+			result = studentDao.deleteStudent(session, no);
+			session.commit();
+		} catch(Exception e) {
+			session.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	@Override
+	public List<Student> selectStudentList() {
+		SqlSession session = getSqlSession();
+		List<Student> list = studentDao.selectStudentList(session);
+		session.close();
+		return list;
+	}
+
+	@Override
+	public List<Map<String, Object>> selectStudentMapList() {
+		SqlSession session = getSqlSession();
+		List<Map<String, Object>> list = studentDao.selectStudentMapList(session);
+		session.close();
+		return list;
+	}
+	
+	
+	
+	
+	
+	
 }
